00f69300ffe4aae9a76a333da514ff276b50ec0b
nazmul609 <nazmulhq101@gmail.com>
2024-04-29 16:37:48 +0600

    updated FindDoctor component for first api call

--- src/components/FindDoctor.jsx
+++ src/components/FindDoctor.jsx
@@ -1,16 +1,35 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import React, { useState,useEffect } from 'react';
+import { Link } from 'react-router-dom'; 
 
-const doctors = [
-  { id: 1, name: "Dr. John Smith", specialty: "Cardiology", phone: "555-555-5555", gender: "Male", img: "https://cdn.pixabay.com/photo/2024/03/25/18/35/ai-generated-8655322_640.png" },
-  { id: 2, name: "Dr. Jane Doe", specialty: "Dermatology", phone: "555-555-5556", gender: "Female", img: "https://cdn.pixabay.com/photo/2024/04/14/03/21/doctor-8694738_640.jpg" },
-  { id: 3, name: "Dr. Jo Willock", specialty: "Cardiology", phone: "555-555-5555", gender: "Male", img: "https://cdn.pixabay.com/photo/2024/03/25/18/35/ai-generated-8655322_640.png" },
-  // ... more doctors
-];
+// const doctors = [
+//   { id: 1, name: "Dr. John Smith", specialty: "Cardiology", phone: "555-555-5555", gender: "Male", img: "https://cdn.pixabay.com/photo/2024/03/25/18/35/ai-generated-8655322_640.png" },
+//   { id: 2, name: "Dr. Jane Doe", specialty: "Dermatology", phone: "555-555-5556", gender: "Female", img: "https://cdn.pixabay.com/photo/2024/04/14/03/21/doctor-8694738_640.jpg" },
+//   { id: 3, name: "Dr. Jo Willock", specialty: "Cardiology", phone: "555-555-5555", gender: "Male", img: "https://cdn.pixabay.com/photo/2024/03/25/18/35/ai-generated-8655322_640.png" },
+//   // ... more doctors
+// ];
 
 function FindDoctor() {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedGender, setSelectedGender] = useState("all");
+  const [doctors, setDoctors] = useState([]); 
+
+  useEffect(() => {
+    fetchDoctors();
+  }, []);
+
+  const fetchDoctors = async () => {
+    try {
+      const response = await fetch('/doctor/allDoctors'); // API Endpoint
+      if (!response.ok) {
+        throw new Error('Failed to fetch doctors');
+      }
+      const data = await response.json();
+      setDoctors(data);
+    } catch (error) {
+      console.error('Error fetching doctors:', error);
+    }
+  };
+
 
   const filteredDoctors = doctors.filter((doctor) => {
     return doctor.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
